96209efe3213696a5548c27c488fe5cad652307b
Rx <0x8084@gmail.com>
2024-10-11 13:40:36 +0700

    init

---
+++ sketch.js
@@ -0,0 +1,143 @@
+//6601012610067 พงษ์จันทร์ จันทร์แจ่ม
+// ต้องการ multiplayer
+// hint
+// timmer
+
+
+let i = 0;
+
+let rows = 4;
+let column = 4;
+
+const gridSize = 80;
+let _numbers = [];
+let offset_y = 0;
+
+
+
+let visibility = [];
+let old_click_number = -1;
+let old_index = -1;
+
+
+
+
+function setup() {
+  createCanvas(600, 800);
+  generateNumbers(rows * column);
+  frameRate(12);
+
+}
+
+// Shuffle function using Fisher-Yates algorithm
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements at index i and j
+  }
+  return array;
+}
+
+function generateNumbers(number_size) {
+  let numbers = [];
+  hides = [];
+  for (i = 0; i < number_size; i += 2) {
+    let num = Math.floor(i / 2) + 1;
+    numbers.push(num);
+    numbers.push(num);
+  }
+
+  _numbers = shuffle(numbers);
+
+
+  for(i=0;i<number_size;i++)
+  {
+    visibility.push(false);
+  }
+
+
+
+}
+
+
+function drawDot(number,x,y)
+{
+
+  let row = 0;
+  for(let o=0;o<number;o++)
+    {
+      row = Math.floor(o/3);
+      offset_y = Math.floor(Math.random() * 3);
+     circle( 10 + gridSize / 4 + (gridSize * x) + (o%3)*8, 
+     10 + gridSize / 2 + (gridSize * y) + Math.floor(o/3)*8 + offset_y, 
+     
+     
+     5);
+
+
+
+    }
+
+
+    
+}
+
+function drawGrids() {
+
+  let id = 1;
+  for (let y = 0; y < column; y++) {
+    for (let x = 0; x < rows; x++) {
+      // Set stroke and fill for the rectangle
+      stroke(0);
+      fill(200);
+      rect(10 + gridSize * x, 10 + gridSize * y, gridSize, gridSize);
+
+      fill(0);
+      textAlign(CENTER, CENTER); // Center the text
+      textSize(34);
+
+      if(visibility[id - 1]) // show dot or not
+      {
+        drawDot(_numbers[id - 1],x,y);
+      }
+
+      //text(_numbers[id - 1], 10 + gridSize / 2 + (gridSize * x), 10 + gridSize / 2 + (gridSize * y));
+      id++;
+    }
+  }
+}
+
+function draw() {
+  background(255); 
+  drawGrids();
+
+}
+
+function mousePressed() {
+
+  let current_index = Math.floor((mouseX - 10) / gridSize) + 4 * Math.floor((mouseY - 10) / gridSize);
+
+  visibility[current_index] = true;
+
+  if(_numbers[current_index] == old_click_number)
+  {
+
+    visibility[old_index] = true;
+    visibility[current_index] = true;
+
+  }
+  else
+  {
+    // visibility[old_index] = false;
+  }
+
+  old_index = current_index;
+  old_click_number = _numbers[current_index];
+
+
+    
+
+
+
+  
+}
